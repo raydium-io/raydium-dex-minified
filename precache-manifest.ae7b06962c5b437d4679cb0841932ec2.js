68811e473031b0093252b63566275909cf46b1ff
Rudy <lengxin5348@gmail.com>
2021-07-08 19:46:06 +0800

    fix samo coin

---
+++ precache-manifest.ae7b06962c5b437d4679cb0841932ec2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3034028ed42780554ee2cba7fbdf3b14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e573f9171c7382379f7",
+    "url": "/static/css/2.d2268191.chunk.css"
+  },
+  {
+    "revision": "886bb34a4f69b4404129",
+    "url": "/static/css/main.2e92e920.chunk.css"
+  },
+  {
+    "revision": "3e573f9171c7382379f7",
+    "url": "/static/js/2.5b019e13.chunk.js"
+  },
+  {
+    "revision": "402e7a7af76a99e4fa92c7a85166f585",
+    "url": "/static/js/2.5b019e13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886bb34a4f69b4404129",
+    "url": "/static/js/main.5fa6b917.chunk.js"
+  },
+  {
+    "revision": "ff8347c2a84f5138b487",
+    "url": "/static/js/runtime-main.766003da.js"
+  },
+  {
+    "revision": "3cdaf3a18accd53c3cbf47846f831a5e",
+    "url": "/static/media/logo.3cdaf3a1.svg"
+  },
+  {
+    "revision": "30b8e744b4353c4579ede0f695f085d4",
+    "url": "/static/media/logo1.30b8e744.svg"
+  }
+]);
